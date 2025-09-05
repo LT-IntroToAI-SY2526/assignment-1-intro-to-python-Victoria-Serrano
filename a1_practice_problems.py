3d42d483ef077ef7999b4aa559287ac5e8e58105
Victoria Serrano <vmserrano1@cps.edu>
2025-09-05 10:19:12 -0500

    added asserts

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -47,7 +47,7 @@ def factorial(n: int) -> int:
     if n == 1:
         return 1
     return n * factorial(n-1)
-    raise NotImplementedError("factorial")
+   
 
 
 T = TypeVar("T")
@@ -132,12 +132,13 @@ def duck_duck_goose(lst: List[str]) -> List[str]:
 # this line causes the nested code to be skipped if the file is imported instead of run
 if __name__ == "__main__":
     assert absolute(-1) == 1, "absolute of -1 failed"
+    assert absolute(4) == 4, "absolute of 4 failed"
     assert factorial(4) == 24, "factorial of 4 failed"
-    assert every_other([1, 2, 3, 4, 5]) == [
-        1,
-        3,
-        5,
-    ], "every_other of [1,2,3,4,5] failed"
+    assert factorial(5) == 120, "factorial of 5 failed"
+    assert factorial(1) == 1, "factorial of 1 failed"
+    assert factorial(0) == 1, "factorial of 0 failed"
+    assert every_other([1, 2, 3, 4, 5]) == [    1,  3, 5,
+     ], "every_other of [1,2,3,4,5] failed"
     assert sum_list([1, 2, 3]) == 6, "sum_list of [1,2,3] failed"
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
     assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
